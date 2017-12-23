3a6afaa741dacc36ddc4f96afa1a187d24d5043e
Paul Rosset <paul@rsst.eu>
2017-12-24 00:59:05 +0100

    read in -> out

--- main/digital_read.c
+++ main/digital_read.c
@@ -7,9 +7,15 @@
 #include <badge_input.h>
 #include <badge_pins.h>
 #include <badge_leds.h>
+#include <esp_log.h>
+
+#include "driver/gpio.h"
 
 #include <font.h>
-
+//bei uns pin 0
+#define READ_GPIO PIN_NUM_EXT_IO_4
+//bei uns pin 1
+#define WRITE_GPIO PIN_NUM_EXT_IO_3
 
 void
 digital_read(void)
@@ -20,6 +26,25 @@ digital_read(void)
 	memset(badge_eink_fb, 0xff, BADGE_EINK_FB_LEN);
 	draw_font(badge_eink_fb, 6, 16, 284, "DigitalRead:",
 		FONT_INVERT);
+	badge_eink_display(badge_eink_fb, DISPLAY_FLAG_LUT(2));
+    gpio_pad_select_gpio(WRITE_GPIO);
+    gpio_pad_select_gpio(READ_GPIO);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(WRITE_GPIO, GPIO_MODE_OUTPUT);
+    gpio_set_direction(READ_GPIO, GPIO_MODE_INPUT);
+    int level = 0;
+    while(1) {
+        /* Blink off (output low) */
+        level = gpio_get_level(READ_GPIO);
+        //vTaskDelay(1000 / portTICK_PERIOD_MS);
+        /* Blink on (output high) */
+        gpio_set_level(WRITE_GPIO, level);
+/*#        if(level)
+#         ets_printf("1.\n");
+#        else
+ #         ets_printf("0.\n");*/
+        //vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 
 	while (1) {
 		// exit on random keypress
